cb99d426421cb6c08f15ba95755df567c710a8d7
Jacob Ormerod <jto888@users.noreply.github.com>
2020-03-05 13:00:56 -0500

    refactor probability
    probability can be calculated by defaults top gate by "bdd" or any gate ID and possible other methods such as "mcub"

---
+++ R/probability.r
@@ -0,0 +1,18 @@
+probability<-function(DF, ft_node=1, method="bdd")  {		
+	ftree.validate(DF)	
+		
+	if(DF$Type[ft_node] < 10)  stop("ft_node must be a gate")	
+		
+	chars_in<-DF$Tag	
+	ints_in<-c(DF$ID, DF$Type, DF$CParent, DF$MOE, DF$EType)	
+	nums_in<-c( DF$PBF, DF$P1, DF$P2)
+	if(!tolower(method) %in% c("bdd", "mcub") stop(paste0("method ", method, " is not recognized"))
+	if(method=="bdd")  {	
+		prob<-.Call( "probability", chars_in, ints_in, nums_in, ft_node=1, PACKAGE = "FaultTreeBDD" )
+	}else{	
+## leaving logical space for eventual mcub calculation		
+#		ret_list<-.Call( "mocus", chars_in, ints_in, nums_in, ft_node=1, PACKAGE = "FaultTreeBDD" )
+#		prob<-ret_list[[3]]
+	}	
+prob		
+}		
