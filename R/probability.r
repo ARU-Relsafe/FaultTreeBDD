eec3aed8efb0d7647b03f1f114bc5d55a24768ed
Jacob Ormerod <jto888@users.noreply.github.com>
2020-03-11 18:40:34 -0400

    added ftree.calc

--- R/probability.r
+++ R/probability.r
@@ -1,8 +1,9 @@
 probability<-function(DF, ft_node=1, method="bdd")  {		
 	ftree.validate(DF)	
 		
-	if(DF$Type[ft_node] < 10)  stop("ft_node must be a gate")	
-		
+	if(DF$Type[which(DF$ID==ft_node)] < 10)  stop("ft_node must be a gate")	
+##	if(DF$Type[which(DF$ID==ft_node)] < 10) browser()
+	
 	chars_in<-DF$Tag	
 	ints_in<-c(DF$ID, DF$Type, DF$CParent, DF$MOE, DF$EType)	
 	nums_in<-c( DF$PBF, DF$P1, DF$P2)
